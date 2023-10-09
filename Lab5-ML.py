88366a6bbc10d935803db6f0e4b36cce10ae577e
GanguChirudeep <144471481+GanguChirudeep@users.noreply.github.com>
2023-10-09 09:37:55 +0530

    Update Lab5-ML.py

--- Lab5-ML.py
+++ Lab5-ML.py
@@ -969,4 +969,35 @@ def test_classifier(classifier, data, targets):
 print("\nTesting the Trained Classifier:")
 test_classifier(mlp, X, y)
 
+#A10
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+
+# Training data for XOR gate
+# XOR gate truth table: inputs and corresponding outputs
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 1, 1, 0])
+
+# Create an MLPClassifier with one hidden layer
+mlp = MLPClassifier(hidden_layer_sizes=(2,), activation='logistic', solver='sgd', learning_rate_init=0.05, max_iter=100)
+
+# Train the classifier
+mlp.fit(X, y)
+
+# Print the trained weights and biases
+print("Trained Weights (Coefs):")
+print(mlp.coefs_)
+print("Trained Biases (Intercepts):")
+print(mlp.intercepts_)
+
+# Test the trained classifier
+def test_classifier(classifier, data, targets):
+    predictions = classifier.predict(data)
+    accuracy = (sum(predictions == targets) / len(targets)) * 100
+    print("Predictions:", predictions)
+    print("Accuracy:", accuracy, "%")
+
+# Test the trained classifier
+print("\nTesting the Trained Classifier:")
+test_classifier(mlp, X, y)
 
